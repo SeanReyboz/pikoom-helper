7fb758551d506cb83b0853b25c3ed2edcf033493
SeanReyboz <seanreyboz@protonmail.com>
2021-10-10 21:25:03 +0200

    Modification du cron job rappel réunion

--- commands/commandes.js
+++ commands/commandes.js
@@ -2,113 +2,113 @@
  * @file commands/commandes.js
  * @description définition d'un tableau d'objet contenant toutes les propriétes
  * de toutes les commandes du bot.
- * 
+ *
  * NOTE: Le nom d'une commande doit impérativement commencer par une lettre
  *       minuscule et correspondre à l'expression régulière suivante:
- *       ^[\w-]{1,32}$ 
+ *       ^[\w-]{1,32}$
  */
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports = [
-	{
-		name: 'ajouter',
-		description: 'Ajouter une deadline.',
-		options: [
-			{
-				name: 'date',
-				description: 'La date de la deadline, au format: <JJ-MM-AAAA>.',
-				required: true,
-				type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
-			},
-			{
-				name: 'titre',
-				description: 'Le titre de la nouvelle deadline.',
-				required: true,
-				type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
-			},
-			{
-				name: 'description',
-				description: 'La description de la nouvelle deadline.',
-				required: true,
-				type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
-			},
-		],
-	},
-	{
-		name: 'supprimer',
-		description: 'Supprimer la deadline spécifiée.',
-		options: [
-			{
-				name: 'deadline-id',
-				description: 'L\'identifiant de la deadline à supprimer.',
-				required: true,
-				type: Discord.Constants.ApplicationCommandOptionTypes.INTEGER,
-			},
-		],
-	},
-	{
-		name: 'modifier',
-		description: 'Modifier une deadline existante.',
-		options: [
-			{
-				name: 'deadline-id',
-				description: "L'identifiant de la deadline à modifier.",
-				required: true,
-				type: Discord.Constants.ApplicationCommandOptionTypes.INTEGER,
-			},
-		],
-	},
-	{
-		name: 'suggestion',
-		description: 'Créer une nouvelle suggestion.',
-		options: [
-			{
-				name: 'idea',
-				description: "Le contenu de la suggestion à ajouter.",
-				required: true,
-				type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
-			},
-		],
-	},
-	{
-		name: 'annonce',
-		description: 'Créer une nouvelle annonce.',
-		options: [
-			{
-				name: 'message',
-				description: "Le contenu de l'annonce à ajouter.",
-				required: true,
-				type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
-			},
-		],
-	},
-	{
-		name: 'reunion',
-		description: 'Annoncer une nouvelle réunion.',
-		options: [
-			{
-				name: 'reunion',
-				description: "Le contenu de la réunion à ajouter.",
-				required: true,
-				type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
-			},
-			{
-				name: 'role',
-				description: "Notifier les personnes du rôle spécifié.",
-				type: Discord.Constants.ApplicationCommandOptionTypes.ROLE,
-			}
-		],
-	},
-	{
-		name: 'afficher',
-		description: 'Afficher le temps restant avant la deadline la plus proche.',
-	},
-	{
-		name: 'aide',
-		description: "Afficher un message d'aide d'utilisation des commandes",
-	},
-	{
-		name: 'test',
-		description: "Une simple commande de test...",
-	},
+  {
+    name: "ajouter",
+    description: "Ajouter une deadline.",
+    options: [
+      {
+        name: "date",
+        description: "La date de la deadline, au format: <JJ-MM-AAAA>.",
+        required: true,
+        type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
+      },
+      {
+        name: "titre",
+        description: "Le titre de la nouvelle deadline.",
+        required: true,
+        type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
+      },
+      {
+        name: "description",
+        description: "La description de la nouvelle deadline.",
+        required: true,
+        type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
+      },
+    ],
+  },
+  {
+    name: "supprimer",
+    description: "Supprimer la deadline spécifiée.",
+    options: [
+      {
+        name: "deadline-id",
+        description: "L'identifiant de la deadline à supprimer.",
+        required: true,
+        type: Discord.Constants.ApplicationCommandOptionTypes.INTEGER,
+      },
+    ],
+  },
+  {
+    name: "modifier",
+    description: "Modifier une deadline existante.",
+    options: [
+      {
+        name: "deadline-id",
+        description: "L'identifiant de la deadline à modifier.",
+        required: true,
+        type: Discord.Constants.ApplicationCommandOptionTypes.INTEGER,
+      },
+    ],
+  },
+  {
+    name: "suggestion",
+    description: "Créer une nouvelle suggestion.",
+    options: [
+      {
+        name: "idea",
+        description: "Le contenu de la suggestion à ajouter.",
+        required: true,
+        type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
+      },
+    ],
+  },
+  {
+    name: "annonce",
+    description: "Créer une nouvelle annonce.",
+    options: [
+      {
+        name: "message",
+        description: "Le contenu de l'annonce à ajouter.",
+        required: true,
+        type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
+      },
+    ],
+  },
+  {
+    name: "reunion",
+    description: "Annoncer une nouvelle réunion.",
+    options: [
+      {
+        name: "reunion",
+        description: "Le contenu de la réunion à ajouter.",
+        required: true,
+        type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
+      },
+      {
+        name: "role",
+        description: "Notifier les personnes du rôle spécifié.",
+        type: Discord.Constants.ApplicationCommandOptionTypes.ROLE,
+      },
+    ],
+  },
+  {
+    name: "afficher",
+    description: "Afficher le temps restant avant la deadline la plus proche.",
+  },
+  {
+    name: "aide",
+    description: "Afficher un message d'aide d'utilisation des commandes",
+  },
+  {
+    name: "test",
+    description: "Quand ca roule...",
+  },
 ];
--- main.js
+++ main.js
@@ -1,216 +1,225 @@
-const Discord = require('discord.js');
-const { Client, Intents } = require('discord.js');
-const Cron = require('node-cron');
-const Sequelize = require('sequelize');
-const moment = require('moment');
-const { TOKEN, botChannelId, guildId } = require('./config.json');
+const Discord = require("discord.js");
+const { Client, Intents } = require("discord.js");
+const Cron = require("node-cron");
+const Sequelize = require("sequelize");
+const moment = require("moment");
+const { TOKEN, botChannelId, guildId } = require("./config.json");
 
-const listeCommandes = require('./commands/commandes');
+const listeCommandes = require("./commands/commandes");
 
 /** Création du client (bot)
- * Intents: 
+ * Intents:
  * Permettent de déterminer quels évènements le bot doit recevoir.
  * Il est impératif de fournir les Intents nécessaire pour la v.14 de
  * discord.js, sans quoi la création du client est impossible.
- * 
- *  - Intents possibles: 
+ *
+ *  - Intents possibles:
  *    voir <https://discord.com/developers/docs/topics/gateway#list-of-intents>
  */
 const client = new Discord.Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] 
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-
 // Base de données
-const sequelize = new Sequelize('database', 'user', 'password', {
-	host: 'localhost',
-	dialect: 'sqlite',
-	logging: false,
-	storage: 'database.sqlite',
+const sequelize = new Sequelize("database", "user", "password", {
+  host: "localhost",
+  dialect: "sqlite",
+  logging: false,
+  storage: "database.sqlite",
 });
 
 // Création du modèle
 // NOTE: aucune clé primaire n'est définie ici, car sequelize en créer une par
 // défaut.
-const Deadlines = sequelize.define('deadlines', {
-	title: Sequelize.STRING,
-	description: Sequelize.TEXT,
-	date: {
-		type: Sequelize.DATE,
-		get() {
-			// Modifier le format de la date retournée...
-			return moment(this.getDataValue('date')).format('DD/MM/YYYY');
-		}
-	},
+const Deadlines = sequelize.define("deadlines", {
+  title: Sequelize.STRING,
+  description: Sequelize.TEXT,
+  date: {
+    type: Sequelize.DATE,
+    get() {
+      // Modifier le format de la date retournée...
+      return moment(this.getDataValue("date")).format("DD/MM/YYYY");
+    },
+  },
 });
 
 // Afficher un message dans la console lorsque le bot est prêt.
-client.once('ready', () => {
-	console.log(`Bot connecté en tant que ${client.user.tag}`);
-	// Vérifier que le modèle existe dans la BDD
-	Deadlines.sync();
+client.once("ready", () => {
+  console.log(`Bot connecté en tant que ${client.user.tag}`);
+  // Vérifier que le modèle existe dans la BDD
+  Deadlines.sync();
 });
 
+client.on("ready", () => {
+  // Afficher un rappel bi-hebdomadaire pour les réunions du lundi et mercredi
+  // RAPPEL: Secondes Minutes Heures Jour-du-Mois Mois Jour-de-la-Semaine !!!
+  Cron.schedule("1 1 19 * * 2,7", () => {
+    client.channels.fetch(botChannelId).then((channel) => {
+      channel.send(
+        ":calendar: __RAPPEL__\nRéunion prévue demain, à la première pause.\n\nN'oubliez pas :heart:"
+      );
+    });
+  });
 
-client.on('ready', () => {
+  Cron.schedule("1 30 9 * * 1", async () => {
+    const closestDeadline = await Deadlines.findOne({
+      order: [["date", "ASC"]],
+    });
+    const deadline = closestDeadline.dataValues;
+    // convertir la date:
+    deadline.date = moment(deadline.date).format("DD/MM/YYYY");
+    client.channels.fetch(botChannelId).then((channel) => {
+      channel.send(
+        `:warning: **RAPPEL DEADLINE !!** :warning:\n\n**${deadline.title}**\n${deadline.description}\n\n> Date: __*${deadline.date}*__\n@everyone`
+      );
+    });
+  });
 
-	// Afficher un rappel bi-hebdomadaire pour les réunions du lundi et mercredi
-	Cron.schedule('1 * 19 * * 2,7', () => {
-		client.channels.fetch(botChannelId).then(channel => {
-			channel.send(":calendar: __RAPPEL__\nRéunion prévue demain, à la première pause.\n\nN'oubliez pas :heart:");
-		});
-	});
+  // Ajout des commandes au bot ----
+  const guild = client.guilds.cache.get(guildId);
+  let commands;
 
-	Cron.schedule('1 30 9 * * 1', async () => {
-		const closestDeadline = await Deadlines.findOne({
-			order: [
-				['date', 'ASC'],
-			],
-		});
-		const deadline = closestDeadline.dataValues;
-		// convertir la date:
-		deadline.date = moment(deadline.date).format('DD/MM/YYYY');
-		client.channels.fetch(botChannelId).then(channel => {
-			channel.send(`:warning: **RAPPEL DEADLINE !!** :warning:\n\n**${deadline.title}**\n${deadline.description}\n\n> Date: __*${deadline.date}*__\n@everyone`);
-		})
-	});
+  if (guild) commands = guild.commands;
+  else commands = client.application.commands;
 
-	// Ajout des commandes au bot ----
-	const guild = client.guilds.cache.get(guildId);
-	let commands;
+  // ajouter la liste des commandes et leurs options respectives.
+  commands.set(listeCommandes);
 
-	if (guild)
-		commands = guild.commands;
-	else
-		commands = client.application.commands;
-
-	// ajouter la liste des commandes et leurs options respectives.
-	commands.set(listeCommandes);
-
-	// Ajout d'un message d'activité ----
-	client.user.setPresence({
-		activities: [{
-			name: '"/aide" pour lister les commandes.',
-		}],
-		status: "online",
-	});
+  // Ajout d'un message d'activité ----
+  client.user.setPresence({
+    activities: [
+      {
+        name: '"/aide" pour lister les commandes.',
+      },
+    ],
+    status: "online",
+  });
 });
-
 
 //  ---   Gestion des Intéractions   ---
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
 
-	const { commandName, options } = interaction;
+  const { commandName, options } = interaction;
 
-	if (commandName === 'ajouter') {
-		// N.B: définir des valeurs par défaut au cas où
-		let date = options.getString('date');
-		const titre = options.getString('titre') || 'Nouvelle Deadline';
-		const desc = options.getString('description') || '';
-		const regex = /\d{2}-\d{2}-\d{4}/;
+  if (commandName === "ajouter") {
+    // N.B: définir des valeurs par défaut au cas où
+    let date = options.getString("date");
+    const titre = options.getString("titre") || "Nouvelle Deadline";
+    const desc = options.getString("description") || "";
+    const regex = /\d{2}-\d{2}-\d{4}/;
 
-		// Vérifier que la date saisie soit validea.
-		if (!regex.test(date)) {
-			return interaction.reply(`La date doit être de la forme JJ-MM-AAAA. Vous avez saisi: ${date}.`);
-		}
+    // Vérifier que la date saisie soit validea.
+    if (!regex.test(date)) {
+      return interaction.reply(
+        `La date doit être de la forme JJ-MM-AAAA. Vous avez saisi: ${date}.`
+      );
+    }
 
-		// Vérification de la cohérence de la date saisie.
-		const elements = date.split('-');
+    // Vérification de la cohérence de la date saisie.
+    const elements = date.split("-");
 
-		// Gérer les erreurs
-		const invalidValueException = (type, min, max, element) => {
-			return interaction.reply(`:warning: **ERREUR**:\n\n> ${type} n'est pas valide.\n\nUne valeur comprise entre ${min} et ${max} attendue, "${element}" reçu.`)
-		}
+    // Gérer les erreurs
+    const invalidValueException = (type, min, max, element) => {
+      return interaction.reply(
+        `:warning: **ERREUR**:\n\n> ${type} n'est pas valide.\n\nUne valeur comprise entre ${min} et ${max} attendue, "${element}" reçu.`
+      );
+    };
 
-		if (elements[0] < 1 || elements[0] > 31) {
-			return invalidValueException("Le jour", 1, 31, elements[0]);
-		}
-		if (elements[1] < 1 || elements[1] > 12) {
-			return invalidValueException("Le mois", 1, 12, elements[1]);
-		}
-		if (elements[2] < 2021 || elements[2] > 2022) {
-			return invalidValueException("L'année", 2021, 2022, elements[2]);
-		}
+    if (elements[0] < 1 || elements[0] > 31) {
+      return invalidValueException("Le jour", 1, 31, elements[0]);
+    }
+    if (elements[1] < 1 || elements[1] > 12) {
+      return invalidValueException("Le mois", 1, 12, elements[1]);
+    }
+    if (elements[2] < 2021 || elements[2] > 2022) {
+      return invalidValueException("L'année", 2021, 2022, elements[2]);
+    }
 
-		// Réassembler la date dans un format supporté par sqlite.
-		date = `${elements[2]}-${elements[1]}-${elements[0]} 12:00:00`;
+    // Réassembler la date dans un format supporté par sqlite.
+    date = `${elements[2]}-${elements[1]}-${elements[0]} 12:00:00`;
 
-		try {
-			const deadline = await Deadlines.create({
-				title: titre,
-				description: desc,
-				date: date,
-			});
-			return interaction.reply(`La deadline intitulée "${titre}" a bien été ajoutée!`);
-		} catch (error) {
-			if (error.name === 'SequelizeUniqueConstraintError') {
-				return interaction.reply("L'identifiant est déjà utilisé.");
-			}
-			return interaction.reply("Oops! Une erreur s'est produite lors de l'ajout de la deadline.");	
-		}
+    try {
+      const deadline = await Deadlines.create({
+        title: titre,
+        description: desc,
+        date: date,
+      });
+      return interaction.reply(
+        `La deadline intitulée "${titre}" a bien été ajoutée!`
+      );
+    } catch (error) {
+      if (error.name === "SequelizeUniqueConstraintError") {
+        return interaction.reply("L'identifiant est déjà utilisé.");
+      }
+      return interaction.reply(
+        "Oops! Une erreur s'est produite lors de l'ajout de la deadline."
+      );
+    }
+  } else if (commandName === "supprimer") {
+    const id = options.getInteger("deadline-id") || 0;
 
-	} else if (commandName === 'supprimer') {
-		const id = options.getInteger('deadline-id') || 0;
+    // Supprimer la deadline d'ID spéficié
+    const rowCount = await Deadlines.destroy({ where: { id: id } });
 
-		// Supprimer la deadline d'ID spéficié
-		const rowCount = await Deadlines.destroy({ where: { id: id } });
+    if (!rowCount)
+      return interaction.reply(`La deadline "${id}" n'existe pas.`);
 
-		if (!rowCount) return interaction.reply(`La deadline "${id}" n'existe pas.`);
+    return interaction.reply(`La deadline "${id}" a bien été supprimée.`);
+  } else if (commandName === "afficher") {
+    let message = ":calendar: **LISTE des DEADLINES** :calendar:";
 
-		return interaction.reply(`La deadline "${id}" a bien été supprimée.`);
+    try {
+      const deadlines = await Deadlines.findAll();
+      const len = deadlines.length;
 
-	} else if (commandName === 'afficher') {
-		let message = ':calendar: **LISTE des DEADLINES** :calendar:';
+      if (len === 0) {
+        return interaction.reply(
+          "Il n'y a aucune deadline (...pour l'instant ;) )"
+        );
+      }
 
-		try {
-			const deadlines = await Deadlines.findAll();
-			const len = deadlines.length;
+      for (let i = 0; i < len; i++) {
+        message += `\n\n---\nDeadline n° ${deadlines[i].id} -- **${deadlines[i].title}**\n\n${deadlines[i].description}\n\n> DEADLINE: __*${deadlines[i].date}*__`;
+      }
+      return interaction.reply(message);
+    } catch (error) {
+      console.log(error);
+      return interaction.reply(
+        "Oops! Quelque chose s'est mal passé lors de la récupération de la liste des deadlines."
+      );
+    }
+  } else if (commandName === "suggestion") {
+    const idea = options.getString("idea") || "Nouvelle suggestion";
 
-			if (len === 0) {
-				return interaction.reply("Il n'y a aucune deadline (...pour l'instant ;) )");
-			}
+    interaction.reply(`:ballot_box: **Suggestion de ${interaction.user} !**\n`);
+    // Puisqu'il est impossible de notifier des rôles dans une réponse d'interaction,
+    // on envoie un autre message à la suite pour notifier @everyone
+    const message = await interaction.channel.send({
+      content: `@everyone \n\n> ${idea}\n\n:white_check_mark: Je valide !\n:x: Je suis contre !`,
+      fetchReply: true,
+    });
+    message.react("✅");
+    message.react("❌");
+  } else if (commandName === "annonce") {
+    const message = options.getString("message") || "Nouvelle annonce.";
 
-			for (let i = 0; i < len; i++) {
-				message += `\n\n---\nDeadline n° ${deadlines[i].id} -- **${deadlines[i].title}**\n\n${deadlines[i].description}\n\n> DEADLINE: __*${deadlines[i].date}*__`
-			}
-			return interaction.reply(message);
+    interaction.reply(`:warning: **Annonce !!!**`);
+    await interaction.channel.send(`\n@everyone\n\n> ${message}`);
+  } else if (commandName === "reunion") {
+    const reunion = options.getString("reunion") || "Nouvelle réunion.";
+    const role = options.getRole("role");
+    const mention = role ? role : "@everyone";
 
-		} catch (error) {
-			console.log(error);
-			return interaction.reply("Oops! Quelque chose s'est mal passé lors de la récupération de la liste des deadlines.");
-		}
-	} else if (commandName === 'suggestion') {
-		const idea = options.getString('idea') || "Nouvelle suggestion";
-
-		interaction.reply(`:ballot_box: **Suggestion de ${interaction.user} !**\n`);
-		// Puisqu'il est impossible de notifier des rôles dans une réponse d'interaction,
-		// on envoie un autre message à la suite pour notifier @everyone
-		const message = await interaction.channel.send({ 
-			content: `@everyone \n\n> ${idea}\n\n:white_check_mark: Je valide !\n:x: Je suis contre !`,
-			fetchReply: true,
-		});
-		message.react('✅');
-		message.react('❌');
-	} else if (commandName === 'annonce') {
-		const message = options.getString('message') || "Nouvelle annonce.";
-
-		interaction.reply(`:warning: **Annonce !!!**`);
-		await interaction.channel.send(`\n@everyone\n\n> ${message}`);
-	} else if (commandName === 'reunion') {
-		const reunion = options.getString('reunion') || "Nouvelle réunion.";
-		const role = options.getRole('role');
-		const mention = (role) ? role : '@everyone';
-
-		interaction.reply(`:date: **Réunion !!!**`);
-		const message = await interaction.channel.send({ 
-			content:`\n${mention}\n\n> ${reunion} \n\n:white_check_mark:  Pour lu !`, 
-			fetchReply: true
-		});
-		message.react('✅');
-	} else if (commandName === 'aide') {
-		return interaction.reply(`Liste des commandes disponibles...
+    interaction.reply(`:date: **Réunion !!!**`);
+    const message = await interaction.channel.send({
+      content: `\n${mention}\n\n> ${reunion} \n\n:white_check_mark:  Pour lu !`,
+      fetchReply: true,
+    });
+    message.react("✅");
+  } else if (commandName === "aide") {
+    return interaction.reply(`Liste des commandes disponibles...
 
 - SYNOPSIS:
 Une commande est de la forme "**/commande**". Elle accepte et/ou requiert parfois des options ou arguments.
@@ -229,27 +238,18 @@ Les arguments requis sont notés "**<argument>**", tandis que les options sont n
 - GÉNÉRAL:
 **/aide** - Afficher ce message d'aide.
 		`);
-	} else if (commandName === 'test') {
-		const closestDeadline = await Deadlines.findOne({
-			order: [
-				['date', 'ASC'],
-			],
-		});
-		const deadline = closestDeadline.dataValues;
-		// convertir la date:
-		deadline.date = moment(deadline.date).format('DD/MM/YYYY');
-		interaction.reply(':warning: **RAPPEL DEADLINE !!** :warning:\n\n');
-		
-		return interaction.channel.send({
-			content: `**${deadline.title}**\n${deadline.description}\n\n> Date: __*${deadline.date}*__\n@everyone`,
-			fetchReply: true,
-		});
-	}
+  } else if (commandName === "test")
+    return interaction.reply(
+      "> Quand ça tourne, ça marche.\n- *Inconnu, 2021*"
+    );
 });
 
 // Connecter le bot à Discord.
-client.login(TOKEN).then(() => {
-	console.log("--> Client connecté...");
-}).catch(error => {
-	console.log(error);
-});
+client
+  .login(TOKEN)
+  .then(() => {
+    console.log("--> Client connecté...");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
